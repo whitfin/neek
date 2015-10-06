c1c200f65cca067643271037b109ff2049c3122a
Isaac Whitfield <iwhitfield@appcelerator.com>
2015-10-06 08:01:28 +0100

    Correct minor build issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,14 @@ module.exports = function(grunt) {
 
   // project configuration
   grunt.initConfig({
+    codeclimate: {
+      main: {
+        options: {
+          file: 'coverage/lcov.info',
+          token: process.env.CODECLIMATE_REPO_TOKEN
+        }
+      }
+    },
     clean: {
       coverage: ['coverage'],
       tmp: ['.tmp/*.txt']
@@ -46,6 +54,7 @@ module.exports = function(grunt) {
   });
 
   // load grunt plugins for modules
+  grunt.loadNpmTasks('grunt-codeclimate-reporter');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
